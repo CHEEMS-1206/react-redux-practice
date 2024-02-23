2a037b814638dd3f261de00c5a7bfa68f0b99a3f
PRIYANSHU SINGH <sentysingh456@gmail.com>
2024-02-23 13:31:55 +0530

    rendered the can ke and ice cream component in the app component

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import "./App.css";
+
+import CakeContainer from "./Components/CakeContainer";
+import IceCreamContainer from "./Components/IceCreamContainer";
+
+import store from "./Components/Redux/Store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      HELLO WORLD !
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        Confectionary
+        <CakeContainer />{" "}
+        {/*using connect, mapStateToProp and mapDispatchToProp */}
+        <IceCreamContainer /> {/*using redux hooks */}
+      </div>
+    </Provider>
   );
 }
 
